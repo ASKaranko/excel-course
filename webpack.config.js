eaa47fc83b41313e6bc83433d6964b937227b37e
Karanko Andrei <andr.karanko@gmail.com>
2020-12-29 19:14:35 +0200

    Finish of base functional framework

--- webpack.config.js
+++ webpack.config.js
@@ -10,92 +10,95 @@ const isDev = !isProd;
 const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
 
 const jsLoaders = () => {
-  const loaders = [
-    {
-      loader: 'babel-loader',
-      options: {
-        presets: [
-          ['@babel/preset-env',
-            {
-              'targets': {
-                'edge': '17',
-                'firefox': '60',
-                'chrome': '67',
-                'safari': '11.1',
-                'ie': '11'
-              },
-              'useBuiltIns': 'usage'
-            }]
-        ]
-      }
-    }
-  ];
+	const loaders = [
+		{
+			loader: 'babel-loader',
+			options: {
+				presets: [
+					['@babel/preset-env',
+						{
+							'targets': {
+								'edge': '17',
+								'firefox': '60',
+								'chrome': '67',
+								'safari': '11.1',
+								'ie': '11'
+							},
+							'useBuiltIns': 'usage'
+						}]
+				],
+				plugins: [
+					["@babel/plugin-proposal-class-properties"]
+				]
+			}
+		}
+	];
 
-  if (isDev) {
-    loaders.push('eslint-loader');
-  }
-  return loaders;
+	if (isDev) {
+		loaders.push('eslint-loader');
+	}
+	return loaders;
 };
 
 module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  mode: 'development',
-  entry: {
-    main: path.resolve(__dirname, './src/index.js'),
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: filename('js'),
-  },
-  resolve: {
-    extensions: ['.js'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@core': path.resolve(__dirname, 'src/core'),
-    },
-  },
-  devtool: isDev ? 'source-map' : false,
-  devServer: {
-    contentBase: path.resolve(__dirname, './dist'),
-    port: 3000,
-    hot: isDev,
-    open: true,
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-      minify: {
-        removeComments: isProd,
-        collapseWhitespace: isProd,
-      },
-    }),
-    new CopyPlugin({
-      patterns: [
-        {from: path.resolve(__dirname, 'src/favicon.ico'),
-          to: path.resolve(__dirname, 'dist'),
-        },
-      ],
-    }),
-    new MiniCssExtractPlugin({
-      filename: filename('css'),
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: jsLoaders(),
-      },
-    ],
-  },
+	context: path.resolve(__dirname, 'src'),
+	mode: 'development',
+	entry: {
+		main: path.resolve(__dirname, './src/index.js'),
+	},
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: filename('js'),
+	},
+	resolve: {
+		extensions: ['.js'],
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			'@core': path.resolve(__dirname, 'src/core'),
+		},
+	},
+	devtool: isDev ? 'source-map' : false,
+	devServer: {
+		contentBase: path.resolve(__dirname, './dist'),
+		port: 3000,
+		hot: isDev,
+		open: true,
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: 'index.html',
+			minify: {
+				removeComments: isProd,
+				collapseWhitespace: isProd,
+			},
+		}),
+		new CopyPlugin({
+			patterns: [
+				{from: path.resolve(__dirname, 'src/favicon.ico'),
+					to: path.resolve(__dirname, 'dist'),
+				},
+			],
+		}),
+		new MiniCssExtractPlugin({
+			filename: filename('css'),
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: jsLoaders(),
+			},
+		],
+	},
 };
