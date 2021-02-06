4fb1cd8fb65a88d2e449e5512a10bbad2ece9198
Karanko Andrei <andr.karanko@gmail.com>
2021-02-06 20:40:43 +0200

    Added unit testing environment and some tests regarding Router and createStore

--- webpack.config.js
+++ webpack.config.js
@@ -11,29 +11,7 @@ const isDev = !isProd;
 const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
 
 const jsLoaders = () => {
-	const loaders = [
-		{
-			loader: 'babel-loader',
-			options: {
-				presets: [
-					['@babel/preset-env',
-						{
-							'targets': {
-								'edge': '17',
-								'firefox': '60',
-								'chrome': '67',
-								'safari': '11.1',
-								'ie': '11'
-							},
-							'useBuiltIns': 'usage'
-						}]
-				],
-				plugins: [
-					["@babel/plugin-proposal-class-properties"]
-				]
-			}
-		}
-	];
+	const loaders = ['babel-loader'];
 
 	if (isDev) {
 		loaders.push('eslint-loader');
